4566f41f9a3cf8b0e1a41baff2565e35f7c250c7
MiriamVF <miriam.verduf@gmail.com>
2021-10-02 14:39:06 +0200

    add html

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
-import "../styles/App.scss";
+import "../styles/Card.scss";
 
 function App() {
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <h1>Rick and Morty</h1>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Busca a tu personaje favorito"
+      />
+      <ul>
+        <li className="card">
+          <img src="" alt={`Foto de nombre`} title={`Foto de nombre`} />
+          <h4>Nombre del personaje</h4>
+          <p>Especie</p>
+        </li>
+      </ul>
     </div>
   );
 }
